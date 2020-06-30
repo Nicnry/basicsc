2eee1de34c194766471292b393d574e883d0e210
Nicolas Henry <nicolas.henry@cpnv.ch>
2020-06-30 19:57:16 +0200

    Finish chapter

---
+++ variables/cmake-build-debug/CMakeFiles/variables.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/variables.dir/main.cpp.o"
+  "variables"
+  "variables.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/variables.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
