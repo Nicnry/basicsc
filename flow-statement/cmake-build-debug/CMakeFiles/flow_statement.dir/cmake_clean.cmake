4114c1937c8c7fabafd42741574ba4be85aafac9
Nicolas Henry <nicolas.henry@cpnv.ch>
2020-06-30 20:46:13 +0200

    Finish chapter

---
+++ flow-statement/cmake-build-debug/CMakeFiles/flow_statement.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flow_statement.dir/main.cpp.o"
+  "flow_statement"
+  "flow_statement.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flow_statement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
