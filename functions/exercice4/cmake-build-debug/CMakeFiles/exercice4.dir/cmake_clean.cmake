b1022b4d654aa5dbba5311fab9fe1a1485818f36
Nicolas Henry <nicolas.henry@cpnv.ch>
2020-08-06 16:31:16 +0200

    Using Local and Global Variables

---
+++ functions/exercice4/cmake-build-debug/CMakeFiles/exercice4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercice4.dir/main.cpp.o"
+  "exercice4"
+  "exercice4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercice4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ functions/exercice4/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+const int POSITION = 10;
+const int ALREADY_COMPUTED = 3;
+
+void print_tenth_fibonacci(){
+    int n_1 = 1;
+    int n_2 = 0;
+    int current = n_1 + n_2;
+    for(int i = ALREADY_COMPUTED; i < POSITION; ++i){
+        n_2 = n_1;
+        n_1 = current;
+        current = n_1 + n_2;
+    }
+    std::cout << current << std::endl;
+};
+
+int main() {
+    std::cout << "Computing the 10th Fibonacci number" << std::endl;
+    print_tenth_fibonacci();
+}
